b995a4c06552010124c47e6ade02a3811746a3ea
daria <dasha.loza24@gmail.com>
2022-01-12 18:24:02 +0300

    Client without Front

--- client/src/components/AppRouter.js
+++ client/src/components/AppRouter.js
@@ -1,13 +1,16 @@
-import React from 'react';
-import{Routes,Route,Redirect} from "react-router-dom";
+import React, {useContext} from 'react';
+import{Routes,Route, Navigate} from "react-router-dom";
 import {authRoutes, publicRoutes} from "../routes";
+import {SHOP_ROUTES} from "../utilis/consts";
+import {Context} from "../index";
 
 
 const AppRouter = () => {
-    const isAuth = false
+    const {user} = useContext(Context)
+    console.log(user)
     return (
         <Routes>
-            {isAuth && authRoutes.map(({path, Component}) =>
+            {user.isAuth && authRoutes.map(({path, Component}) =>
                  //<Route key={path} path={path} component={Component} exact/>
                  <Route exact path={path} element={<Component/>}/>
             )}
--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, {createContext} from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import UserStore from "./store/UserStore";
+import BookStore from "./store/BookStore";
 
+
+export const Context = createContext(null)
 ReactDOM.render(
-    <App />,
+    <Context.Provider value={{
+        user: new UserStore(),
+        book: new BookStore()
+    }}>
+        <App />
+    </Context.Provider>,
   document.getElementById('root')
 );
 
